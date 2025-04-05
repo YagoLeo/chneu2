73dd42ca26a97d05a8d417d700e2ec43f25ca974
CC <faaa@live.com>
2025-04-05 20:40:32 +0800

    update

--- .next/types/app/layout.ts
+++ .next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: D:\Projects\chneu2\src\app\layout.tsx
+// File: /Users/admin/Projects/chneu2/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
